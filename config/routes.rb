17f5f7beb6ada39d0d38288ca0742db585f92b27
Daisuke Adachi <maurois@mac.com>
2010-11-27 20:33:40 +0900

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
 
   get "pages/about"
+
+  get "users/new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -15,6 +19,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
